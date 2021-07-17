3d88fb8970ccbd692e372bc40f58191683a4389d
Edward Hibbert <edward@ehibbert.org.uk>
2021-07-17 08:49:22 +0100

    Decode settings field.

--- go.mod
+++ go.mod
@@ -8,4 +8,5 @@ require (
 	github.com/gofiber/fiber v1.14.6
 	github.com/gofiber/fiber/v2 v2.15.0
 	github.com/jinzhu/gorm v1.9.16
+	gorm.io/datatypes v1.0.1
 )
--- group/group.go
+++ group/group.go
@@ -1,6 +1,7 @@
 package group
 
 import (
+	"encoding/json"
 	"github.com/freegle/iznik-server-go/database"
 	"github.com/gofiber/fiber/v2"
 	"github.com/jinzhu/gorm"
@@ -11,27 +12,27 @@ import (
 type Group struct {
 	gorm.Model
 	id                   uint
-	Nameshort            string  `json:"nameshort"`
-	Namefull             string  `json:"namefull"`
-	Namedisplay          string  `json:"namedisplay"`
-	Settings             string  `json:"settings"` // TODO decode
-	Type                 string  `json:"type"`
-	Region               string  `json:"region"`
-	Logo                 string  `json:"logo"`
-	Publish              int     `json:"publish"`
-	Onhere               int     `json:"onhere"`
-	Ontn                 int     `json:"ontn"`
-	Membercount          int     `json:"membercount"`
-	Modcount             int     `json:"modcount"`
-	Lat                  float32 `json:"lat"`
-	Lng                  float32 `json:"lng"`
-	Profile              string  `json:"profile"`
-	Cover                string  `json:"cover"`
-	Onmap                int     `json:"onmap"`
-	Tagline              string  `json:"tagline"`
-	Description          string  `json:"description"`
-	Contactmail          string  `json:"contactmail"`
-	Fundingtarget        int     `json:"fundingtarget"`
+	Nameshort            string          `json:"nameshort"`
+	Namefull             string          `json:"namefull"`
+	Namedisplay          string          `json:"namedisplay"`
+	Settings             json.RawMessage `json:"settings"` // This is JSON stored in the DB as a string.
+	Type                 string          `json:"type"`
+	Region               string          `json:"region"`
+	Logo                 string          `json:"logo"`
+	Publish              int             `json:"publish"`
+	Onhere               int             `json:"onhere"`
+	Ontn                 int             `json:"ontn"`
+	Membercount          int             `json:"membercount"`
+	Modcount             int             `json:"modcount"`
+	Lat                  float32         `json:"lat"`
+	Lng                  float32         `json:"lng"`
+	Profile              string          `json:"profile"`
+	Cover                string          `json:"cover"`
+	Onmap                int             `json:"onmap"`
+	Tagline              string          `json:"tagline"`
+	Description          string          `json:"description"`
+	Contactmail          string          `json:"contactmail"`
+	Fundingtarget        int             `json:"fundingtarget"`
 	Affiliationconfirmed time.Time
 	//'affiliationconfirmedby',
 	//'mentored',
@@ -52,6 +53,6 @@ func GetGroup(c *fiber.Ctx) error {
 	id := c.Params("id")
 	db := database.DBConn
 	var group Group
-	db.Unscoped().Find(&group, id)
+	db.Debug().Unscoped().Find(&group, id)
 	return c.JSON(group)
 }
