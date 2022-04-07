da1a92aac6ce26119ebe390f2eb8fadc45d908cc
Edward Hibbert <edward@ehibbert.org.uk>
2022-04-07 10:37:54 +0100

    Mask phone numbers and emails.

--- message/message.go
+++ message/message.go
@@ -7,11 +7,15 @@ import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"os"
+	"regexp"
 	"strconv"
 	"time"
 )
 
 const INTERESTED = "Interested"
+
+const EMAIL_REGEXP = "[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}\b"
+const PHONE_REGEXP = "[0-9]{4,}"
 
 type Message struct {
 	ID                 uint64              `json:"id" gorm:"primary_key"`
@@ -58,13 +62,17 @@ func GetMessage(c *fiber.Ctx) error {
 	// Protect anonymity of poster a bit.
 	message.Lat, message.Lng = utils.Blur(message.Lat, message.Lng, utils.BLUR_USER)
 
+	// Remove confidential info.
+	var er = regexp.MustCompile(EMAIL_REGEXP)
+	message.Textbody = er.ReplaceAllString(message.Textbody, "***@***.com")
+	var ep = regexp.MustCompile(PHONE_REGEXP)
+	message.Textbody = ep.ReplaceAllString(message.Textbody, "***")
+
 	// Get the paths.
 	for i, a := range message.MessageAttachments {
 		message.MessageAttachments[i].Path = "https://" + os.Getenv("IMAGE_ARCHIVED_DOMAIN") + "/img_" + strconv.FormatUint(a.ID, 10) + ".jpg"
 		message.MessageAttachments[i].Paththumb = "https://" + os.Getenv("IMAGE_ARCHIVED_DOMAIN") + "/timg_" + strconv.FormatUint(a.ID, 10) + ".jpg"
 	}
-
-	// TODO Mask phone numbers etc.
 
 	return c.JSON(message)
 }
