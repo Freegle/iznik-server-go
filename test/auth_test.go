325cb752e13dc2231b1d7a6fca5fd7b3d347aa88
Edward Hibbert <edward@ehibbert.org.uk>
2022-06-20 08:57:19 +0100

    Test login with JWT.

---
+++ test/auth_test.go
@@ -0,0 +1,38 @@
+package test
+
+import (
+	json2 "encoding/json"
+	"github.com/freegle/iznik-server-go/database"
+	"github.com/freegle/iznik-server-go/router"
+	user2 "github.com/freegle/iznik-server-go/user"
+	"github.com/gofiber/fiber/v2"
+	"github.com/stretchr/testify/assert"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestAuth(t *testing.T) {
+	app := fiber.New()
+	database.InitDatabase()
+	db := database.DBConn
+	router.SetupRoutes(app)
+
+	// Find a user.
+	var user user2.User
+	db.First(&user)
+
+	// Get their JWT. This matches the PHP code.
+	token := GetToken(user.ID)
+
+	// Get the logged in user.
+	resp, _ := app.Test(httptest.NewRequest("GET", "/api/user?jwt="+token, nil))
+	assert.Equal(t, 200, resp.StatusCode)
+	var user2 user2.User
+	json2.Unmarshal(rsp(resp), &user2)
+
+	// Should match the user we tried to log in as.
+	assert.Equal(t, user2.ID, user.ID)
+
+	// Should see memberships.
+	assert.Greater(t, len(user2.Memberships), 0)
+}
--- test/testUtils.go
+++ test/testUtils.go
@@ -1,9 +1,13 @@
 package test
 
 import (
+	"fmt"
+	"github.com/golang-jwt/jwt/v4"
 	"io"
 	"net/http"
+	"os"
 	"strings"
+	"time"
 )
 
 func rsp(response *http.Response) []byte {
@@ -11,4 +15,16 @@ func rsp(response *http.Response) []byte {
 	io.Copy(buf, response.Body)
 	//fmt.Println(buf.String())
 	return []byte(buf.String())
+}
+
+func GetToken(id uint64) string {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"id":  fmt.Sprint(id),
+		"exp": time.Now().Unix() + 30*60,
+	})
+
+	// Sign and get the complete encoded token as a string using the secret
+	tokenString, _ := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+
+	return tokenString
 }
