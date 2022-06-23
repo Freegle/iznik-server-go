af61e8ff3b90f0226cfda920ddecd292196297eb
Edward Hibbert <edward@ehibbert.org.uk>
2022-06-23 16:05:59 +0100

    Fix race.

--- user/userInfo.go
+++ user/userInfo.go
@@ -24,6 +24,7 @@ func GetUserUinfo(id uint64) UserInfo {
 	db := database.DBConn
 
 	var info UserInfo
+	var mu sync.Mutex
 
 	info.Replies = 0
 	info.Reneged = 0
@@ -38,19 +39,25 @@ func GetUserUinfo(id uint64) UserInfo {
 	go func() {
 		defer wg.Done()
 		// No need to check on the chat room type as we can only get messages of type Interested in a User2User chat.
-		db.Raw("SELECT COUNT(DISTINCT refmsgid) AS replies FROM chat_messages WHERE userid = ? AND date > ? AND refmsgid IS NOT NULL AND type = ?", id, start, utils.CHAT_MESSAGE_INTERESTED).Scan(&info)
+		res := db.Raw("SELECT COUNT(DISTINCT refmsgid) AS replies FROM chat_messages WHERE userid = ? AND date > ? AND refmsgid IS NOT NULL AND type = ?", id, start, utils.CHAT_MESSAGE_INTERESTED)
+		mu.Lock()
+		defer mu.Unlock()
+		res.Scan(&info)
 	}()
 
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		db.Raw("SELECT COUNT(DISTINCT(messages_reneged.msgid)) AS reneged FROM messages_reneged WHERE userid = ? AND timestamp > ?", id, start).Scan(&info)
+		res := db.Raw("SELECT COUNT(DISTINCT(messages_reneged.msgid)) AS reneged FROM messages_reneged WHERE userid = ? AND timestamp > ?", id, start).Scan(&info)
+		mu.Lock()
+		defer mu.Unlock()
+		res.Scan(&info)
 	}()
 
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		db.Raw("SELECT COUNT(DISTINCT messages_by.msgid) AS collected FROM messages_by "+
+		res := db.Raw("SELECT COUNT(DISTINCT messages_by.msgid) AS collected FROM messages_by "+
 			"INNER JOIN messages ON messages.id = messages_by.msgid "+
 			"INNER JOIN chat_messages ON chat_messages.refmsgid = messages.id AND messages.type = ? AND chat_messages.type = ? "+
 			"INNER JOIN messages_groups ON messages_groups.msgid = messages.id WHERE chat_messages.userid = ? AND messages_by.userid = ? AND messages_by.userid != messages.fromuser AND messages_groups.arrival >= ?",
@@ -58,7 +65,10 @@ func GetUserUinfo(id uint64) UserInfo {
 			utils.CHAT_MESSAGE_INTERESTED,
 			id,
 			id,
-			start).Scan(&info)
+			start)
+		mu.Lock()
+		defer mu.Unlock()
+		res.Scan(&info)
 	}()
 
 	wg.Add(1)
@@ -88,6 +98,8 @@ func GetUserUinfo(id uint64) UserInfo {
 
 				db.ScanRows(rows, &cr)
 
+				mu.Lock()
+
 				switch cr.Type {
 				case utils.OFFER:
 					info.Offers += cr.Count
@@ -102,6 +114,8 @@ func GetUserUinfo(id uint64) UserInfo {
 						info.Openwanteds += cr.Count
 					}
 				}
+
+				mu.Unlock()
 			}
 		}
 	}()
