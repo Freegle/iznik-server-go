71a51791c01ea204b460e95f61573cf5fe148a41
Edward Hibbert <edward@ehibbert.org.uk>
2022-11-13 13:59:08 +0000

    Return story image.

--- story/story.go
+++ story/story.go
@@ -3,25 +3,54 @@ package story
 import (
 	"github.com/freegle/iznik-server-go/database"
 	"github.com/gofiber/fiber/v2"
+	"os"
+	"strconv"
 	"time"
 )
 
+type StoryImage struct {
+	ID        uint64 `json:"id"`
+	Path      string `json:"path"`
+	PathThumb string `json:"paththumb"`
+}
+
 type Story struct {
-	ID       uint64     `json:"id" gorm:"primary_key"`
-	Userid   uint64     `json:"userid"`
-	Date     *time.Time `json:"date"`
-	Headline string     `json:"headline"`
-	Story    string     `json:"story"`
+	ID            uint64      `json:"id" gorm:"primary_key"`
+	Userid        uint64      `json:"userid"`
+	Date          *time.Time  `json:"date"`
+	Headline      string      `json:"headline"`
+	Story         string      `json:"story"`
+	Imageid       uint64      `json:"imageid"`
+	Imagearchived bool        `json:"-"`
+	Image         *StoryImage `json:"image"`
 }
 
 func Single(c *fiber.Ctx) error {
 	var s Story
 
 	db := database.DBConn
-	db.Raw("SELECT * FROM users_stories WHERE id = ? AND public = 1", c.Params("id")).Scan(&s)
+	db.Raw("SELECT users_stories.*, users_stories_images.id AS imageid, users_stories_images.archived AS imagearchived FROM users_stories "+
+		"LEFT JOIN users_stories_images ON users_stories_images.storyid = users_stories.id "+
+		"WHERE users_stories.id = ? AND public = 1", c.Params("id")).Scan(&s)
 
 	if s.ID == 0 {
 		return fiber.NewError(fiber.StatusNotFound, "Not found")
+	}
+
+	if s.Imageid > 0 {
+		if s.Imagearchived {
+			s.Image = &StoryImage{
+				ID:        s.Imageid,
+				Path:      "https://" + os.Getenv("IMAGE_ARCHIVED_DOMAIN") + "/simg_" + strconv.FormatUint(s.Imageid, 10) + ".jpg",
+				PathThumb: "https://" + os.Getenv("IMAGE_ARCHIVED_DOMAIN") + "/tsimg_" + strconv.FormatUint(s.Imageid, 10) + ".jpg",
+			}
+		} else {
+			s.Image = &StoryImage{
+				ID:        s.Imageid,
+				Path:      "https://" + os.Getenv("USER_SITE") + "/simg_" + strconv.FormatUint(s.Imageid, 10) + ".jpg",
+				PathThumb: "https://" + os.Getenv("USER_SITE") + "/tsimg_" + strconv.FormatUint(s.Imageid, 10) + ".jpg",
+			}
+		}
 	}
 
 	return c.JSON(s)
