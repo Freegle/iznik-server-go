f9f9021fcee397f77bf4b1675bc985f7f1bb2b10
Edward Hibbert <edward@ehibbert.org.uk>
2023-02-09 16:06:10 +0000

    User added date wrong

--- user/user.go
+++ user/user.go
@@ -245,7 +245,7 @@ func GetUserById(id uint64, myid uint64) User {
 			settingsq = "settings, "
 		}
 
-		if !db.Raw("SELECT users.id, firstname, lastname, fullname, lastaccess, systemrole, relevantallowed, newslettersallowed, bouncing, "+settingsq+
+		if !db.Raw("SELECT users.id, firstname, lastname, fullname, lastaccess, users.added, systemrole, relevantallowed, newslettersallowed, bouncing, "+settingsq+
 			"(CASE WHEN spam_users.id IS NOT NULL AND spam_users.collection = 'Spammer' THEN 1 ELSE 0 END) AS spammer, "+
 			"CASE WHEN systemrole IN ('Moderator', 'Support', 'Admin') AND JSON_EXTRACT(users.settings, '$.showmod') IS NULL THEN 1 ELSE JSON_EXTRACT(users.settings, '$.showmod') END AS showmod "+
 			"FROM users LEFT JOIN spam_users ON spam_users.userid = users.id "+
