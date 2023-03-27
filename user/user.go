432cb4c143783a4215a2142d5f89f38ff6f0df09
Edward Hibbert <edward@ehibbert.org.uk>
2023-03-27 12:45:40 +0100

    Return microvolunteering status for logged in user.

--- user/user.go
+++ user/user.go
@@ -60,6 +60,7 @@ type User struct {
 	Relevantallowed    bool            `json:"relevantallowed"`
 	Newslettersallowed bool            `json:"newslettersallowed"`
 	Bouncing           bool            `json:"bouncing"`
+	Trustlevel         string          `json:"trustlevel"`
 }
 
 type Tabler interface {
@@ -117,6 +118,7 @@ func GetUser(c *fiber.Ctx) error {
 			user.Relevantallowed = false
 			user.Newslettersallowed = false
 			user.Bouncing = false
+			user.Trustlevel = ""
 
 			if user.ID == id {
 				return c.JSON(user)
@@ -249,7 +251,7 @@ func GetUserById(id uint64, myid uint64) User {
 			settingsq = "settings, "
 		}
 
-		err := db.Raw("SELECT users.id, firstname, lastname, fullname, lastaccess, users.added, systemrole, relevantallowed, newslettersallowed, bouncing, "+settingsq+
+		err := db.Raw("SELECT users.id, firstname, lastname, fullname, lastaccess, users.added, systemrole, relevantallowed, newslettersallowed, trustlevel, bouncing, "+settingsq+
 			"(CASE WHEN spam_users.id IS NOT NULL AND spam_users.collection = 'Spammer' THEN 1 ELSE 0 END) AS spammer, "+
 			"CASE WHEN systemrole IN ('Moderator', 'Support', 'Admin') AND JSON_EXTRACT(users.settings, '$.showmod') IS NULL THEN 1 ELSE JSON_EXTRACT(users.settings, '$.showmod') END AS showmod "+
 			"FROM users LEFT JOIN spam_users ON spam_users.userid = users.id "+
