6a90f03e2595592795843da0d20897ab7253e2ae
Edward Hibbert <edward@ehibbert.org.uk>
2023-04-16 15:27:09 +0100

    Validate session some more.

--- user/authMiddleware.go
+++ user/authMiddleware.go
@@ -12,14 +12,13 @@ type Config struct{}
 func NewAuthMiddleware(config Config) fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		var userIdInDB uint64
-		var sessionIdInDB uint64
 		userIdInJWT, sessionIdInJWT, _ := getJWTFromRequest(c)
 
 		var wg sync.WaitGroup
 
 		if userIdInJWT > 0 {
 			// We have a valid JWT with a user id in it.  But is the user id still in our DB?  And do they still
-			// have an active session?
+			// have the same active session active session?
 			wg.Add(1)
 			db := database.DBConn
 
@@ -27,24 +26,16 @@ func NewAuthMiddleware(config Config) fiber.Handler {
 				defer wg.Done()
 
 				// We have a uid.  Check if the user is still present in the DB.
-				db.Raw("SELECT id FROM users WHERE id = ? LIMIT 1;", userIdInJWT).Scan(&userIdInDB)
+				db.Raw("SELECT users.id FROM users INNER JOIN sessions ON users.id = sessions.userid WHERE users.id = ? AND sessions.id = ? LIMIT 1;", userIdInJWT, sessionIdInJWT).Scan(&userIdInDB)
 			}()
 
 			wg.Add(1)
-
-			go func() {
-				// Check if the session is still present in the DB.  If we have explicitly logged out, then it
-				// will have been removed and therefore our JWT is invalid.
-				defer wg.Done()
-
-				db.Raw("SELECT id FROM sessions WHERE id = ? LIMIT 1;", sessionIdInJWT).Scan(&sessionIdInDB)
-			}()
 		}
 
 		ret := c.Next()
 		wg.Wait()
 
-		if userIdInJWT > 0 && (userIdInDB != userIdInJWT || sessionIdInDB != sessionIdInJWT) {
+		if userIdInJWT > 0 && (userIdInDB != userIdInJWT) {
 			// We were passed a user ID in the JWT, but it's not present in the DB.  This means that the user has
 			// sent an invalid JWT.  Return an error.
 			fmt.Println("Invalid user in JWT", userIdInJWT, userIdInDB)
