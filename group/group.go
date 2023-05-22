97babb0d47ee684cf6e1f2c4d326557f5451b641
Edward Hibbert <edward@ehibbert.org.uk>
2023-05-22 13:10:30 +0100

    Group profile images not showing.

--- group/group.go
+++ group/group.go
@@ -106,7 +106,7 @@ func GetGroup(c *fiber.Ctx) error {
 
 		if found {
 			if group.GroupProfile.ID > 0 {
-				group.GroupProfileStr = "https://" + os.Getenv("USER_SITE") + "/gimg_" + strconv.FormatUint(group.GroupProfile.ID, 10) + ".jpg"
+				group.GroupProfileStr = "https://" + os.Getenv("IMAGE_DOMAIN") + "/gimg_" + strconv.FormatUint(group.GroupProfile.ID, 10) + ".jpg"
 			}
 
 			if len(group.Namefull) > 0 {
