6ddb36467b099e7fe14607cf1a189cc4c80e19e5
Edward Hibbert <edward@ehibbert.org.uk>
2023-07-05 15:41:59 +0100

    Don't return withdrawn messages on a group.

--- group/groupMessages.go
+++ group/groupMessages.go
@@ -18,7 +18,12 @@ func GetGroupMessages(c *fiber.Ctx) error {
 	now := time.Now()
 	then := now.AddDate(0, 0, -31)
 
-	db.Raw("SELECT msgid FROM messages_groups WHERE groupid = ? AND arrival >= ? AND collection = ? AND deleted = 0 ORDER BY arrival DESC", id, then.Format(time.RFC3339), utils.COLLECTION_APPROVED).Pluck("msgid", &ret)
+	// We want to return messages which have no outcome or are successful (which will be shown by the client as
+	// freegled) but not withdrawn mmessages.
+	db.Raw("SELECT messages_groups.msgid FROM messages_groups "+
+		"LEFT JOIN messages_outcomes ON messages_outcomes.msgid = messages_groups.msgid "+
+		"WHERE groupid = ? AND arrival >= ? AND collection = ? AND deleted = 0 AND (messages_outcomes.id IS NULL OR messages_outcomes.outcome IN (?, ?)) "+
+		"ORDER BY arrival DESC", id, then.Format(time.RFC3339), utils.COLLECTION_APPROVED, utils.OUTCOME_TAKEN, utils.OUTCOME_RECEIVED).Pluck("msgid", &ret)
 
 	return c.JSON(ret)
 }
