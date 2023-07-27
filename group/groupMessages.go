156e61a9da3034e214afb5aa2f2bfdf2d9c974af
Edward Hibbert <edward@ehibbert.org.uk>
2023-07-27 21:44:40 +0100

    Always show messages from logged in user on the group page, even if pending.

--- group/groupMessages.go
+++ group/groupMessages.go
@@ -2,6 +2,7 @@ package group
 
 import (
 	"github.com/freegle/iznik-server-go/database"
+	"github.com/freegle/iznik-server-go/user"
 	"github.com/freegle/iznik-server-go/utils"
 	"github.com/gofiber/fiber/v2"
 	"strconv"
@@ -10,6 +11,7 @@ import (
 
 func GetGroupMessages(c *fiber.Ctx) error {
 	var ret []uint64
+	myid := user.WhoAmI(c)
 
 	id, _ := strconv.ParseUint(c.Params("id"), 10, 64)
 
@@ -19,11 +21,12 @@ func GetGroupMessages(c *fiber.Ctx) error {
 	then := now.AddDate(0, 0, -31)
 
 	// We want to return messages which have no outcome or are successful (which will be shown by the client as
-	// freegled) but not withdrawn mmessages.
+	// freegled) but not withdrawn messages.  We also want to add in any messages of our own.
 	db.Raw("SELECT messages_groups.msgid FROM messages_groups "+
 		"LEFT JOIN messages_outcomes ON messages_outcomes.msgid = messages_groups.msgid "+
-		"WHERE groupid = ? AND arrival >= ? AND collection = ? AND deleted = 0 AND (messages_outcomes.id IS NULL OR messages_outcomes.outcome IN (?, ?)) "+
-		"ORDER BY arrival DESC", id, then.Format(time.RFC3339), utils.COLLECTION_APPROVED, utils.OUTCOME_TAKEN, utils.OUTCOME_RECEIVED).Pluck("msgid", &ret)
+		"INNER JOIN messages ON messages.id = messages_groups.msgid "+
+		"WHERE groupid = ? AND arrival >= ? AND (collection = ? OR messages.fromuser = ?) AND deleted = 0 AND (messages_outcomes.id IS NULL OR messages_outcomes.outcome IN (?, ?)) "+
+		"ORDER BY arrival DESC", id, then.Format(time.RFC3339), utils.COLLECTION_APPROVED, myid, utils.OUTCOME_TAKEN, utils.OUTCOME_RECEIVED).Pluck("msgid", &ret)
 
 	return c.JSON(ret)
 }
