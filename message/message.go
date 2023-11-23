c0be9a87d4b79c75ed0c9e9b6844cd02b36e507c
Edward Hibbert <edward@ehibbert.org.uk>
2023-11-23 15:48:12 +0000

    Introduce primary photo.

--- message/message.go
+++ message/message.go
@@ -125,7 +125,7 @@ func GetMessagesByIds(myid uint64, ids []string) []Message {
 			wg.Add(1)
 			go func() {
 				defer wg.Done()
-				db.Raw("SELECT id, msgid, archived FROM messages_attachments WHERE msgid = ? ORDER BY id ASC", id).Scan(&messageAttachments)
+				db.Raw("SELECT id, msgid, archived FROM messages_attachments WHERE msgid = ? ORDER BY `primary` DESC, id ASC", id).Scan(&messageAttachments)
 			}()
 
 			var messageReply []MessageReply
