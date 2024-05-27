74c0a86b1dd71180eddc8e7e54867b44050f7adf
Edward Hibbert <edward@ehibbert.org.uk>
2024-05-27 12:42:20 +0100

    Return attachment mods for Uploadcare correctly.

--- message/messageAttachment.go
+++ message/messageAttachment.go
@@ -1,16 +1,18 @@
 package message
 
+import "github.com/freegle/iznik-server-go/misc"
+
 func (MessageAttachment) TableName() string {
 	return "messages_attachments"
 }
 
 type MessageAttachment struct {
-	ID           uint64 `json:"id" gorm:"primary_key"`
-	Msgid        uint64 `json:"-"`
-	Path         string `json:"path"`
-	Paththumb    string `json:"paththumb"`
-	Archived     int    `json:"archived"`
-	Externaluid  string `json:"externaluid"`
-	Externalurl  string `json:"externalurl"`
-	Externalmods string `json:"externalmods"`
+	ID           uint64              `json:"id" gorm:"primary_key"`
+	Msgid        uint64              `json:"-"`
+	Path         string              `json:"path"`
+	Paththumb    string              `json:"paththumb"`
+	Archived     int                 `json:"archived"`
+	Externaluid  string              `json:"externaluid"`
+	Externalurl  string              `json:"externalurl"`
+	Externalmods misc.AttachmentMods `json:"externalmods"`
 }
