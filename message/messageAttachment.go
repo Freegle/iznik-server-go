bf3a95afdf885f13523eb7eab818921e7d9b11fa
Edward Hibbert <edward@ehibbert.org.uk>
2024-05-27 12:59:03 +0100

    Return attachment mods for Uploadcare correctly.

--- message/messageAttachment.go
+++ message/messageAttachment.go
@@ -1,18 +1,20 @@
 package message
 
-import "github.com/freegle/iznik-server-go/misc"
+import (
+	"encoding/json"
+)
 
 func (MessageAttachment) TableName() string {
 	return "messages_attachments"
 }
 
 type MessageAttachment struct {
-	ID           uint64              `json:"id" gorm:"primary_key"`
-	Msgid        uint64              `json:"-"`
-	Path         string              `json:"path"`
-	Paththumb    string              `json:"paththumb"`
-	Archived     int                 `json:"archived"`
-	Externaluid  string              `json:"externaluid"`
-	Externalurl  string              `json:"externalurl"`
-	Externalmods misc.AttachmentMods `json:"externalmods"`
+	ID           uint64          `json:"id" gorm:"primary_key"`
+	Msgid        uint64          `json:"-"`
+	Path         string          `json:"path"`
+	Paththumb    string          `json:"paththumb"`
+	Archived     int             `json:"archived"`
+	Externaluid  string          `json:"externaluid"`
+	Externalurl  string          `json:"externalurl"`
+	Externalmods json.RawMessage `json:"externalmods"`
 }
--- misc/attachmentMods.go
+++
@@ -1,8 +0,0 @@
-package misc
-
-// We need to define the mods that we support, so that we can decode the JSON we get back from the database, and
-// then re-encode it when we send it back.
-type AttachmentMods struct {
-	format string `json:"format"`
-	rotate int    `json:"rotate"`
-}
