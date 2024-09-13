679eabd13d8a84f28750f3710626d5d5aaca521f
Edward Hibbert <edward@ehibbert.org.uk>
2024-09-13 09:03:46 +0100

    WIP Get Go API running on Netlify

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 set -euxo pipefail
 
 mkdir -p "$(pwd)/functions"
+go mod download github.com/Azure/go-ansiterm
+go mod tidy
 GOBIN=$(pwd)/functions go install ./...
 chmod +x "$(pwd)"/functions/*
 go env
