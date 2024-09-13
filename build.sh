2158009806d7e559adf58d3d08254d2962131a84
Edward Hibbert <edward@ehibbert.org.uk>
2024-09-13 10:16:59 +0100

    WIP Get Go API running on Netlify

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
 # For deploying on Netlify
 go version
+
+# Don't understand why these are necessary, because (presumaly) I don't understand at least one of Go modules/Netlify
+go mod download github.com/aws/aws-lambda-go
+go mod download github.com/awslabs/aws-lambda-go-api-proxy
+go mod download github.com/freegle/iznik-server-go
+go mod download github.com/go-sql-driver/mysql
+go mod download github.com/gofiber/fiber/v2
+
 go build -o functions/gateway main.go
