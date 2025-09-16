47c31e5161fc114fee53c1483564db1d71e758a1
edwh <edward@ehibbert.org.uk>
2025-09-16 12:58:05 +0100

    WIP FreegleDocker CircleCI

--- test/main_test.go
+++ test/main_test.go
@@ -24,8 +24,8 @@ func init() {
 		return c.Redirect("/swagger/index.html", 302)
 	})
 
-	// Serve swagger static files from ./swagger directory
-	app.Static("/swagger", "./swagger", fiber.Static{
+	// Serve swagger static files from ../swagger directory (relative to test directory)
+	app.Static("/swagger", "../swagger", fiber.Static{
 		Index: "index.html",
 	})
 
